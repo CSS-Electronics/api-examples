12f14c0d6186779209e3b340bf1bb6f92ffe43ee
mfcss <mf@csselectronics.com>
2023-01-05 15:45:34 +0100

    Minor edits to process_data.py

--- examples/data-processing/process_data.py
+++ examples/data-processing/process_data.py
@@ -8,30 +8,36 @@ from utils import setup_fs, load_dbc_files, restructure_data, add_custom_sig, Pr
 # specify devices to process (from local/S3), DBC files, start time and optionally passwords
 devices = ["LOG/958D2219"]
 
-dbc_paths = ["dbc_files/CSS-Electronics-SAE-J1939-DEMO.dbc"]
+# dbc_paths = ["dbc_files/CSS-Electronics-SAE-J1939-DEMO.dbc"]
+
+dbc_paths = ["dbc_files/CSS-Electronics-SAE-J1939-DEMO.dbc", "dbc_files/CSS-Electronics-SAE-J1939-DEMO.dbc"]
+
 start = datetime(year=2020, month=1, day=13, hour=0, tzinfo=timezone.utc)
+stop = datetime(year=2030, month=1, day=1, hour=0, tzinfo=timezone.utc)
+
 pw = {"default": "password"}
 
 # setup filesystem (local/S3), load DBC files and list log files for processing
 fs = setup_fs(s3=False, key="", secret="", endpoint="", passwords=pw)
 db_list = load_dbc_files(dbc_paths)
-log_files = canedge_browser.get_log_files(fs, devices, start_date=start, passwords=pw)
+log_files = canedge_browser.get_log_files(fs, devices, start_date=start, stop_date=stop, passwords=pw)
 print(f"Found a total of {len(log_files)} log files")
 
 # --------------------------------------------
 # perform data processing of each log file (e.g. evaluation of signal stats vs. thresholds)
 proc = ProcessData(fs, db_list, signals=[])
-df_phys_all = pd.DataFrame()
+df_phys_all = []
 
 for log_file in log_files:
     df_raw, device_id = proc.get_raw_data(log_file, passwords=pw)
     df_phys = proc.extract_phys(df_raw)
     proc.print_log_summary(device_id, log_file, df_phys)
 
-    test_signal_threshold(df_phys=df_phys, signal="EngineSpeed", threshold=800)
+    # test_signal_threshold(df_phys=df_phys, signal="EngineSpeed", threshold=800)
 
-    df_phys_all = df_phys_all.append(df_phys)
+    df_phys_all.append(df_phys)
 
+df_phys_all = pd.concat(df_phys_all,ignore_index=False).sort_index()
 
 # --------------------------------------------
 # example: Add a custom signal
@@ -43,6 +49,6 @@ def ratio(s1, s2):
 
 # --------------------------------------------
 # example: resample and restructure data (parameters in columns)
-df_phys_join = restructure_data(df_phys=df_phys_all, res="1S", full_col_names=True)
+df_phys_join = restructure_data(df_phys=df_phys_all, res="1S")
 df_phys_join.to_csv("output_joined.csv")
 print("\nConcatenated DBC decoded data:\n", df_phys_join)
